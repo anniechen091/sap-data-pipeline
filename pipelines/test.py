25bba32ccc947da39bbdefaa9a0e03e445ad8b46
Annie <104024376+anniechen091@users.noreply.github.com>
2025-11-21 16:10:24 -0800

    update files

--- pipelines/test.py
+++
@@ -1,8 +0,0 @@
-all_files = os.listdir(folder_path)
-txt_files = [os.path.join(folder_path, f) for f in all_files if f.endswith('.txt')]
-dfs = []
-for file in txt_files:
-    df = pd.read_csv(file, delimiter="\t", skiprows=2, dtype=dtype_dict)
-    df = df.iloc[:-3,2:]
-    df.columns = df.columns.str.strip()
-    df['Pstng Date'] = pd.to_datetime(df['Pstng Date'], format='%m/%d/%Y')
